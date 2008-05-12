ca17ed3c85f259da03015ad2aaf18ee0a8655848
Mattias Persson <mattias@neotechnology.com>
2008-05-12 11:03:54 +0000

    Some additions, can test with NativeStore as well.

--- src/test/java/org/neo4j/rdf/sail/Playbacker.java
+++ src/test/java/org/neo4j/rdf/sail/Playbacker.java
@@ -2,21 +2,29 @@ package org.neo4j.rdf.sail;
 
 public class Playbacker extends NeoTestCase
 {
-	public static void main( String[] args ) throws Exception
-	{
+//	public static void main( String[] args ) throws Exception
+//	{
 //		final NeoService neo = new EmbeddedNeo( "var/neo" );
-//		final IndexService indexService = new LuceneIndexService( neo );
+//		final IndexService indexService = new CachingLuceneIndexService( neo );
 //		VerboseQuadStore store = new VerboseQuadStore( neo, indexService );
 //		File fileToPlayback = new File(
 ////			"playback/load-atoll-data.log"
 ////			"playback/atoll-activity.log"
-////			"playback/atoll-activity-full.log"
+////			"playback/atoll-activity-full2.log"
 //            "playback/relatedthings.log"
 //			);
-//		Sail baseSail = new NeoSail( neo, store );
+//		
+//		Sail baseSail = null;
+//		
+//		NeoSail neoSail = new NeoSail( neo, store );
+//		//neoSail.setIterateResults( true );
+//		baseSail = neoSail;
+//		
+////		baseSail = new NativeStore( new File( "var/ns" ) );
+//		
 //		try
 //		{
-//			for ( int i = 0; i < 20; i++ )
+//			for ( int i = 0; i < 10; i++ )
 //			{
 //				SimpleTimer timer = null;
 //				Sail sail = null;
@@ -43,9 +51,9 @@ public class Playbacker extends NeoTestCase
 //			indexService.shutdown();
 //			neo.shutdown();
 //		}
-	}
-	
-	public void testNothing()
-	{
-	}
+//	}
+//	
+//	public void testNothing()
+//	{
+//	}
 }
