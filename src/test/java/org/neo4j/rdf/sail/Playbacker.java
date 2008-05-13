f3a4ff88b34b995c8a15114b2533a4e1830814b1
Mattias Persson <mattias@neotechnology.com>
2008-05-13 19:26:38 +0000

    Just some inproved playbacker :)

--- src/test/java/org/neo4j/rdf/sail/Playbacker.java
+++ src/test/java/org/neo4j/rdf/sail/Playbacker.java
@@ -1,30 +1,33 @@
 package org.neo4j.rdf.sail;
 
+// Why is this a test case, well I don't know... don't really care.
 public class Playbacker extends NeoTestCase
 {
 //	public static void main( String[] args ) throws Exception
 //	{
-//		final NeoService neo = new EmbeddedNeo( "var/neo" );
-//		final IndexService indexService = new CachingLuceneIndexService( neo );
-//		VerboseQuadStore store = new VerboseQuadStore( neo, indexService );
 //		File fileToPlayback = new File(
-////			"playback/load-atoll-data.log"
-////			"playback/atoll-activity.log"
-////			"playback/atoll-activity-full2.log"
-//            "playback/relatedthings.log"
+//			"playback/load-atoll-data.log"
+//		//	"playback/atoll-activity.log"
+//		//	"playback/atoll-activity-full.log"
+//		//	"playback/relatedthings.log"
 //			);
 //		
 //		Sail baseSail = null;
 //		
+//		// The Neo Rdf Store
+//		final NeoService neo = new EmbeddedNeo( "var/neo" );
+//		final IndexService indexService = new CachingLuceneIndexService( neo );
+//		VerboseQuadStore store = new VerboseQuadStore( neo, indexService );
 //		NeoSail neoSail = new NeoSail( neo, store );
-//		//neoSail.setIterateResults( true );
 //		baseSail = neoSail;
 //		
-////		baseSail = new NativeStore( new File( "var/ns" ) );
+//		// The Native Store
+//		// baseSail = new NativeStore( new File( "var/ns" ) );
 //		
 //		try
 //		{
-//			for ( int i = 0; i < 10; i++ )
+//			baseSail.initialize();
+//			for ( int i = 0; i < 1; i++ )
 //			{
 //				SimpleTimer timer = null;
 //				Sail sail = null;
