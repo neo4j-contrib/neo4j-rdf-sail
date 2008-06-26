41e3922d163b048aa4bdccb061db08fa207a044c
Mattias Persson <mattias@neotechnology.com>
2008-06-26 14:56:43 +0000

    Added lap/total

--- src/test/java/org/neo4j/rdf/sail/SimpleTimer.java
+++ src/test/java/org/neo4j/rdf/sail/SimpleTimer.java
@@ -2,21 +2,37 @@ package org.neo4j.rdf.sail;
 
 public class SimpleTimer
 {
-	private long startTime;
+	private long totalTime;
+	private int counter;
+	private long currentStartTime;
 	
 	public SimpleTimer()
 	{
-		this.startTime = System.currentTimeMillis();
+		this.currentStartTime = System.currentTimeMillis();
+	}
+	
+	public void newLap()
+	{
+		long time = System.currentTimeMillis() - currentStartTime;
+		printTime( time, "Lap time" );
+		totalTime += time;
+		counter++;
+		currentStartTime = System.currentTimeMillis();
 	}
 	
 	public void end()
 	{
-		long time = System.currentTimeMillis() - startTime;
+		newLap();
+		printTime( totalTime / counter, "Average time" );
+	}
+	
+	private void printTime( long time, String title )
+	{
 		int seconds = ( int ) ( time / 1000 );
 		int minutes = seconds / 60;
 		seconds = seconds % 60;
 		int millis = ( int ) ( time % 1000 );
-		System.out.println( "Time: " + minutes + " min " +
+		System.out.println( title + ": " + minutes + " min " +
 			seconds + "," + millis + " sec" );
 	}
 }
